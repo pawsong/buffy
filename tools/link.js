fdb3c2630c9c02864848fa13ec1849b4a9879f64
Giff Song <giff.song@gmail.com>
2015-11-10 00:27:24 +0900

    Run link task on npm install

--- tools/link.js
+++ tools/link.js
@@ -4,6 +4,10 @@ const exec = require('child_process').execSync;
 const cwd = process.cwd();
 
 const pkg = require(`${cwd}/package.json`);
+if (!pkg.link) {
+  return;
+}
+
 pkg.link.forEach(path => {
   exec(`npm link ${path}`, { cwd });
 });
