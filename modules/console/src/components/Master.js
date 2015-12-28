34dcff006fe04569ba4f31f1cc751bb382c21a32
Giff Song <giff.song@gmail.com>
2015-12-28 16:41:03 +0900

    Fit gmae view element to its parent

--- modules/console/src/components/Master.js
+++ modules/console/src/components/Master.js
@@ -106,8 +106,8 @@ const styles = {
     margin: 'auto',
     position: 'absolute',
     top: 0, left: 0, bottom: 0, right: 0,
-    width: '600px',
-    height: '480px',
+    width: '100%',
+    height: '100%',
     overflow: 'hidden',
   },
 };
