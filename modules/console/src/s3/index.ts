a8ff4bc5a71030c8683106f89d292c972aac5dd0
Giff Song <giff.song@gmail.com>
2016-01-18 15:22:32 +0900

    Remove unused file

--- modules/console/src/s3/index.ts
+++
@@ -1,9 +0,0 @@
-import * as conf from '@pasta/config';
-import * as AWS from 'aws-sdk';
-
-export default new AWS.S3({
-  accessKeyId: conf.awsAccessKeyId,
-  secretAccessKey: conf.awsSecretKey,
-  region: conf.awsS3Region,
-  signatureVersion: 'v4',
-}) as any;
