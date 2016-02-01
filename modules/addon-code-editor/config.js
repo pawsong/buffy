f0923700749db7bd655852a903bebfccc080499a
Giff Song <giff.song@gmail.com>
2016-02-01 20:11:05 +0900

    Update jspm config

--- modules/addon-code-editor/config.js
+++ modules/addon-code-editor/config.js
@@ -21,7 +21,7 @@ System.config({
   },
 
   map: {
-    "@pasta/core": "github:@pasta/core@1.0.0",
+    "@pasta/core": "npm:@pasta/core@1.0.0",
     "babel": "npm:babel-core@5.8.35",
     "babel-runtime": "npm:babel-runtime@5.8.35",
     "core-js": "npm:core-js@1.2.6",
@@ -41,6 +41,11 @@ System.config({
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
     },
+    "npm:@pasta/core@1.0.0": {
+      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
+      "fbemitter": "npm:fbemitter@2.0.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
@@ -52,6 +57,13 @@ System.config({
       "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:fbemitter@2.0.1": {
+      "fbjs": "npm:fbjs@0.6.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:fbjs@0.6.1": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
