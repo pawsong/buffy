4b7954310effe48efdb3046a6798f8cb4f115615
Giff Song <giff.song@gmail.com>
2016-02-26 12:44:32 +0900

    Move external modules into code editor directory

--- modules/addon-code-editor/src/client/blockly/index.ts
+++ modules/addon-code-editor/src/client/blockly/index.ts
@@ -2,24 +2,24 @@ const execute = window['execScript'] || (data => window['eval'].call(window, dat
 
 // Blockly loader script registers global bjects with 'var' syntax,
 // so we have to explicitly set global properties again
-execute(`${require('raw!blockly/blockly_compressed.js')};${[
+execute(`${require('raw!../../../externals/blockly/blockly_compressed.js')};${[
   'goog',
   'Blockly',
 ].map(varName => `window.${varName}=${varName}`)}`);
 
-require('script!blockly/blocks_compressed.js');
-require('script!blockly/javascript_compressed.js');
-require('script!blockly/msg/js/en.js');
+require('script!../../../externals/blockly/blocks_compressed.js');
+require('script!../../../externals/blockly/javascript_compressed.js');
+require('script!../../../externals/blockly/msg/js/en.js');
 
 const Blockly = window['Blockly'];
 export { Blockly };
 
 // JS-Interpreter does not have 'package.json' file, so use relative path
 if (process.env.NODE_ENV === 'development') {
-  window['acorn'] = require('../../../../JS-Interpreter/acorn');
-  require('script!../../../../JS-Interpreter/interpreter');
+  window['acorn'] = require('../../../externals/JS-Interpreter/acorn');
+  require('script!../../../externals/JS-Interpreter/interpreter');
 } else {
-  require('script!../../../../JS-Interpreter/acorn_interpreter');
+  require('script!../../../externals/JS-Interpreter/acorn_interpreter');
 }
 
 const Interpreter = window['Interpreter'];
