1c9b6bc071053d56a7cf4fd53c777da2ccf9f931
Giff Song <giff.song@gmail.com>
2016-03-19 06:09:32 +0900

    Preserve deprecated message

--- modules/console/scripts/updateLocaleData.js
+++ modules/console/scripts/updateLocaleData.js
@@ -1,5 +1,7 @@
 import {sync as globSync} from 'glob';
 const fs = require('fs');
+const difference = require('lodash/difference');
+const concat = require('lodash/concat');
 
 const MESSAGES_PATTERN = `${__dirname}/../build/dev/client/messages/**/*.json`;
 const LANG_DIR         = `${__dirname}/../src/messages/`;
@@ -29,10 +31,13 @@ export default function updateLocaleData() {
     delete require.cache[require.resolve(filepath)];
 
     const messages = require(filepath).default;
-    const content =
-      'export default {\n' +
-      ids.map(id => `  '${id}': '${messages[id] || ''}', // ${defaultMessages[id]}`).join('\n') +
-      '\n}\n';
+    const localeIds = Object.keys(messages);
+    const missingIds = difference(localeIds, ids);
+
+    const content = `export default {\n${concat(
+      ids.map(id => `  '${id}': '${messages[id] || ''}', // ${defaultMessages[id]}`),
+      missingIds.map(id => `  /* [Warning] Message not in use */ '${id}': '${messages[id] || ''}',`)
+    ).join('\n')}\n}\n`
 
     if (oldContent !== content) fs.writeFileSync(filepath, content, 'utf8');
   });
