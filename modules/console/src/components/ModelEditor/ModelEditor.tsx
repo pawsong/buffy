34a902e676fb166e68ac54f83fc26651dfd86861
Giff Song <giff.song@gmail.com>
2016-06-03 14:34:27 +0900

    [ModelEditor] Hide overflow

--- modules/console/src/components/ModelEditor/ModelEditor.tsx
+++ modules/console/src/components/ModelEditor/ModelEditor.tsx
@@ -75,6 +75,7 @@ import fileReducer from './reducers/file';
 const styles = {
   root: {
     top: 0, left: 0, bottom: 0, right: 0,
+    overflow: 'hidden',
   },
   canvas: {
     position: 'absolute',
