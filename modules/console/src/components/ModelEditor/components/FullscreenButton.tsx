13d812df5793c02282fd30cd0207d99da0c7f95d
Giff Song <giff.song@gmail.com>
2016-06-06 21:08:11 +0900

    [ModelEditor] Move fullscreen button to bottom right

--- modules/console/src/components/ModelEditor/components/FullscreenButton.tsx
+++ modules/console/src/components/ModelEditor/components/FullscreenButton.tsx
@@ -5,54 +5,38 @@ import * as Colors from 'material-ui/styles/colors';
 const cloneDeep = require('lodash/cloneDeep');
 const update = require('react-addons-update');
 
+const styles = {
+  root: {
+    position: 'absolute',
+    right: 20,
+    bottom: 30,
+  },
+};
+
 interface FullscreenButtonProps extends React.Props<FullscreenButton> {
   onTouchTap: () => any;
   fullscreen: boolean;
 }
 
 class FullscreenButton extends React.Component<FullscreenButtonProps, {}> {
-  static contextTypes = {
-    muiTheme: React.PropTypes.object,
-  } as any
-
-  static childContextTypes = {
-    muiTheme: React.PropTypes.object,
-  };
-
-  muiTheme: MuiTheme;
-
-  constructor(props, context) {
-    super(props, context);
-
-    this.muiTheme = update(context.muiTheme, {
-      rawTheme: { spacing: { iconSize: { $set: 36 } } },
-    });
-  }
-
-  getChildContext() {
-    return { muiTheme: this.muiTheme };
-  }
-
   render() {
     return (
       <IconButton
         onTouchTap={this.props.onTouchTap}
-        tooltipStyles={{ left: 5 }}
-        iconStyle={{ color: Colors.white }}
+        iconStyle={{
+          color: Colors.white,
+          transform: 'scale(1.32)',
+        }}
         style={styles.root} iconClassName="material-icons" tooltipPosition="bottom-center"
         tooltip={this.props.fullscreen ? 'Fullscreen exit' : 'Fullscreen'}
+        tooltipStyles={{
+          top: 30,
+        }}
       >
         {this.props.fullscreen ? 'fullscreen_exit' : 'fullscreen'}
       </IconButton>
     );
   }
 }
-
-const styles = {
-  root: {
-    position: 'absolute',
-    bottom: 10,
-  },
-};
 
 export default FullscreenButton;
