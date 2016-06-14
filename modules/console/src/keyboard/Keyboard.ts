cdbc197415e7d86b8b135f0eab3be3fca1a5209b
Giff Song <giff.song@gmail.com>
2016-06-14 17:23:26 +0900

    Reset keyboard state on visibility change

--- modules/console/src/keyboard/Keyboard.ts
+++ modules/console/src/keyboard/Keyboard.ts
@@ -1,3 +1,16 @@
+let visibilityChange;
+if (__CLIENT__) {
+  if (typeof document.hidden !== 'undefined') { // Opera 12.10 and Firefox 18 and later support
+    visibilityChange = 'visibilitychange';
+  } else if (typeof document['mozHidden'] !== 'undefined') {
+    visibilityChange = 'mozvisibilitychange';
+  } else if (typeof document['msHidden'] !== 'undefined') {
+    visibilityChange = 'msvisibilitychange';
+  } else if (typeof document['webkitHidden'] !== 'undefined') {
+    visibilityChange = 'webkitvisibilitychange';
+  }
+}
+
 const SHIFT = 16;
 
 class Keyboard {
@@ -6,6 +19,11 @@ class Keyboard {
     this.pressed = {};
     document.addEventListener('keydown', this.handleKeyDown, false);
     document.addEventListener('keyup', this.handleKeyUp, false);
+    document.addEventListener(visibilityChange, this.handleVisibilityChanage, false);
+  }
+
+  private handleVisibilityChanage = () => {
+    this.pressed = {};
   }
 
   private handleKeyDown = (e: KeyboardEvent) => {
@@ -23,6 +41,7 @@ class Keyboard {
   dispose() {
     document.removeEventListener('keydown', this.handleKeyDown, false);
     document.removeEventListener('keyup', this.handleKeyUp, false);
+    document.removeEventListener(visibilityChange, this.handleVisibilityChanage, false);
   }
 }
 
