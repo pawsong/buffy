d85014d14d45576d664b2bdeb6014373adc7f3c7
Giff Song <giff.song@gmail.com>
2016-07-26 05:43:38 +0900

    Ditch API call in favor of using Component

--- modules/console/src/screens/Blog/screens/BlogIndex/BlogIndexHandler.tsx
+++ modules/console/src/screens/Blog/screens/BlogIndex/BlogIndexHandler.tsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router';
 import {List, ListItem} from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
 import { FormattedDate } from 'react-intl';
-import { defineMessages, injectIntl, InjectedIntlProps } from 'react-intl';
+import { defineMessages, InjectedIntlProps } from 'react-intl';
 import {BlogPostDocument} from '../../types';
 
 import { preloadApi, connectApi, ApiCall, get } from '../../../../api';
@@ -12,30 +12,31 @@ const anonProfilePicture = require('file!../../../../ic_pets_black_24dp_2x.png')
 
 interface HandlerProps {
   posts: ApiCall<BlogPostDocument[]>;
-  intl?: InjectedIntlProps;
 }
 
 @preloadApi(() => ({
   posts: get(`${CONFIG_API_SERVER_URL}/blog-posts`),
 }))
 @connectApi()
-@injectIntl
 class BlogIndexHandler extends React.Component<HandlerProps, {}> {
   renderPostList() {
     const listItems = this.props.posts.result && this.props.posts.result.map(post => {
       const picture = post.author.picture ? `${__CDN_BASE__}/${post.author.picture}` : anonProfilePicture;
 
-      const date = this.props.intl.formatDate(new Date(post.createdAt), {
-        year: 'numeric',
-        month: 'short',
-        day: 'numeric',
-      });
-
       return (
         <ListItem
           key={post.id}
           primaryText={post.title}
-          secondaryText={date}
+          secondaryText={
+            <div>
+              <FormattedDate
+                value={new Date(post.createdAt)}
+                year='numeric'
+                month='short'
+                day='numeric'
+              />
+            </div>
+          }
           leftAvatar={<Avatar src={picture} />}
           containerElement={<Link to={`/blog/${post.slug}`} />}
         />
