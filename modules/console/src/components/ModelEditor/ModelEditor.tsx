d90c50d49396d9d7360aa602ef0bd40f495062d3
Giff Song <giff.song@gmail.com>
2016-09-01 21:42:42 +0900

    [ModelEditor] Ignore key events from inputs

--- modules/console/src/components/ModelEditor/ModelEditor.tsx
+++ modules/console/src/components/ModelEditor/ModelEditor.tsx
@@ -204,6 +204,14 @@ const inlineStyles = {
   },
 };
 
+function isTargetInput(e: any): boolean {
+  return e.target.type == 'textarea' || e.target.type == 'text' ||
+         e.target.type == 'number' || e.target.type == 'email' ||
+         e.target.type == 'password' || e.target.type == 'search' ||
+         e.target.type == 'tel' || e.target.type == 'url' ||
+         e.target.isContentEditable;
+}
+
 @pure
 @injectIntl
 @connectTarget({
@@ -310,6 +318,7 @@ class ModelEditor extends React.Component<ModelEditorProps, ContainerStates> {
 
   private handleKeyDown = (e: KeyboardEvent) => {
     if (!this.focused) return;
+    if (isTargetInput(e)) return;
 
     e.preventDefault();
 
