9362e1e27e3d16ef090e78108fba780f67812646
Giff Song <giff.song@gmail.com>
2016-09-01 17:19:20 +0900

    Use palette for voxel mesh export

--- modules/console/src/components/ModelEditor/io/mesh.ts
+++ modules/console/src/components/ModelEditor/io/mesh.ts
@@ -20,12 +20,45 @@ export function exportMeshFile(model: ndarray.Ndarray): ExportResult {
 }
 
 export function exportRawMeshFile(model: ndarray.Ndarray): ExportResult {
-  const mesh = mesher(model);
+  const { shape } = model;
+
+  const palette = new Map<number, number>();
+  const paletteColors = [
+    0x00000000, // Palette 0 means empty voxel
+  ];
+
+  const matrix = ndarray(new Uint16Array(shape[0] * shape[1] * shape[2]), shape);
+
+  for (let i = 0; i < shape[0]; ++i) {
+  for (let j = 0; j < shape[1]; ++j) {
+  for (let k = 0; k < shape[2]; ++k) {
+    const c = model.get(i, j, k);
+    if (c === 0) continue;
+
+    let idx = palette.get(c);
+    if (!idx) {
+      idx = paletteColors.length;
+      if (idx > 0x7fff) throw new Error('Exceeds palette size limit');
+
+      paletteColors.push(c);
+      palette.set(c, idx);
+    }
+    matrix.set(i, j, k, idx);
+  }
+  }
+  }
+
+  const paletteBin = new Uint32Array(paletteColors.length);
+  for (let i = 1, len = paletteColors.length; i < len; ++i) {
+    paletteBin[i] = 0xff000000 | paletteColors[i];
+  }
+
   const data: Uint8Array = msgpack.encode({
-    buffer: pako.deflate(model.data.buffer),
-    shape: model.shape,
-    stride: model.stride,
-    offset: model.offset,
+    palette: paletteBin.buffer, // There is not much benefit from pako
+    buffer: pako.deflate(matrix.data.buffer),
+    shape: matrix.shape,
+    stride: matrix.stride,
+    offset: matrix.offset,
   });
   return {
     result: new Uint8Array(data),
